8a725b4147d17a599122355148cc79d5b2758e5f
GoZSY <13207508051@163.com>
2016-06-08 20:43:10 +0800

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,37 @@
+/****************************************************************************************
+从后向前匹配，遇到“.”或相等就向前移动，遇到“*”就分段
+*****************************************************************************************/
+class WildMatch {
+public:
+    bool chkWildMatch(string A, int lena, string B, int lenb) 
+    {
+        // write code here
+        int i = lena-1;
+        int j = lenb-1;
+        while(i >= 0 && j>=0)
+        {
+            if(A[i] == B[j] || B[j]=='.')
+            {
+                i--;
+                j--;
+            }
+            else if(B[j]=='*')
+            {
+                lenb = j;
+                j--;
+            }
+            else
+            {
+                i = i-j+lenb-2;
+                j = lenb-1;
+            }
+        }
+        if(j < 0)
+            return 1;
+        else 
+            return 0;
+        
+    }
+};
+
+
